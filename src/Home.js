f60576e4fc3b9e3ca117d13bc91c35a40541f634
Harsha-Datascience22 <harshamodina@gmail.com>
2026-09-05 21:59:29 +0530

    addednew features

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ function Home(){
     const[foods,setFoods]=useState([]);
     
       useEffect(()=>{
-        fetch("http://localhost:5000/foods")
+        fetch("http://localhost:3001/foods")
         .then(res=>res.json())
         .then(data=>setFoods(data))
       },[]);
