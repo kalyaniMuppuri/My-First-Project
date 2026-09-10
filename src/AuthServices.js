02832111157e54234ba3b7b6d9b04a75a321dd6f
kalyani <Kalyani.m1205@gmail.com>
2026-09-10 19:48:13 +0530

    upload all pages

--- src/AuthServices.js
+++ src/AuthServices.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL='http://localhost:3001/users';
+const API_URL='http://localhost:3000/users';
 
 export const registerUser=async({username,email,password})=>{
     try{
